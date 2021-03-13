913307848cbef1515bdb1023476f4e02a4552878
umesh <64860725+umeshbalajis@users.noreply.github.com>
2021-03-13 13:26:53 +0530

    Update SampleProgram.java

--- SampleProgram.java
+++ SampleProgram.java
@@ -4,17 +4,20 @@ public class SampleProgram {
     public static void main(String[] args) {
         
         int num1, num2, sum;
-        Scanner sc = new Scanner(System.in);
+	 num1=10;
+	 num2=20;
+	    
+        //Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter First Number: ");
-	System.out.println("***** ***** ******* ");
-        num1 = sc.nextInt();
+        //System.out.println("Enter First Number: ");
+	//System.out.println("***** ***** ******* ");
+        //num1 = sc.nextInt();
         
-        System.out.println("Enter Second Number: ");
-	System.out.println("***** ****** ******* ");
-        num2 = sc.nextInt();
+        //System.out.println("Enter Second Number: ");
+	//System.out.println("***** ****** ******* ");
+        //num2 = sc.nextInt();
         
-        sc.close();
+        //sc.close();
         sum = num1 + num2;
         System.out.println("Sum of these numbers: "+sum);
 
